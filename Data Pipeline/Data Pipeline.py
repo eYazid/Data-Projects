73237b55962c2c09766ed2520d49d73864615a33
eYazid <elyazid.boumhaoud@gmail.com>
2025-05-20 00:37:01 +0100

    Update Pipeline

--- Data Pipeline/Data Pipeline.py
+++ Data Pipeline/Data Pipeline.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import sqlite3
+import logging
 import numpy as np
 from datetime import datetime
 from bs4 import BeautifulSoup
@@ -22,43 +23,65 @@ def extract(url, table_attribs):
         target_df.columns = target_df.columns.droplevel(1)
         target_df.columns = ["Country/Territory", "UN region", "GDP_USD_millions", "Year"]
     else:
-         df = pd.DataFrame(columns=["Country", "UN region", "GDP_USD_millions", "Year"])
+         target_df = pd.DataFrame(columns=["Country", "UN region", "GDP_USD_millions", "Year"])
          soup = BeautifulSoup(html_content, "html.parser")
          tables = soup.find_all("table")
          rows = tables[2].find_all("tr")
          for row in rows:
              col = row.find_all("td")
              if col and col[0].find("a"):
-                df = pd.concat([df, pd.DataFrame([{"Country": col[0].a.contents[0], "UN region": col[1].a.contents[0], "GDP_USD_millions": col[2].contents[0], "Year": col[2].contents[0]}])], ignore_index=True)
+                target_df = pd.concat([target_df, pd.DataFrame([{"Country": col[0].a.contents[0], "UN region": col[1].a.contents[0], "GDP_USD_millions": col[2].contents[0], "Year": col[2].contents[0]}])], ignore_index=True)
     return target_df
 
 def transform(df):
-    ''' This function converts the GDP information from Currency
-    format to float value, transforms the information of GDP from
-    USD (Millions) to USD (Billions) rounding to 2 decimal places.
-    The function returns the transformed dataframe.'''
-
+    df["GDP_USD_millions"].apply(lambda x: np.round(float("".join(x.replace('—', "0").split(",")))/1_000,2))
+    df.rename(columns={"GDP_USD_millions": "GDP_USD_billions"}, inplace=True)
     return df
 
 def load_to_csv(df, csv_path):
-    ''' This function saves the final dataframe as a `CSV` file 
-    in the provided path. Function returns nothing.'''
+    df.to_csv(csv_path, index=False)
 
 def load_to_db(df, sql_connection, table_name):
-    ''' This function saves the final dataframe as a database table
-    with the provided name. Function returns nothing.'''
+    df.to_sql(table_name, sql_connection, if_exists='append', index=False)
 
 def run_query(query_statement, sql_connection):
-    ''' This function runs the stated query on the database table and
-    prints the output on the terminal. Function returns nothing. '''
+    pd.read_sql(query_statement, sql_connection)
 
 def log_progress(message):
-    ''' This function logs the mentioned message at a given stage of the code execution to a log file. Function returns nothing'''
+    timestamp_format = '%Y-%h-%d-%H:%M:%S'
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format) 
+    with open("./etl_project_log.log","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')
 
-''' Here, you define the required entities and call the relevant 
-functions in the correct order to complete the project. Note that this
-portion is not inside any function.'''
 
 if __name__ == "__main__":
-        extract(url,0)
+        log_progress('Preliminaries complete. Initiating ETL process')
+
+        df = extract(url, table_attribs)
+
+        log_progress('Data extraction complete. Initiating Transformation process')
+
+        df = transform(df)
+
+        log_progress('Data transformation complete. Initiating loading process')
+
+        load_to_csv(df, csv_path)
+
+        log_progress('Data saved to CSV file')
+
+        sql_connection = sqlite3.connect('World_Economies.db')
+
+        log_progress('SQL Connection initiated.')
+
+        load_to_db(df, sql_connection, table_name)
+
+        log_progress('Data loaded to Database as table. Running the query')
+
+        query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+        run_query(query_statement, sql_connection)
+
+        log_progress('Process Complete.')
+
+        sql_connection.close()
 
